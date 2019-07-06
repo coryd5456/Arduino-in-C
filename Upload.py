9fb2c3d6ff6ad766ea319c2562cef9afe9ac995b
coryd5456 <31057817+coryd5456@users.noreply.github.com>
2019-07-06 00:17:37 -0700

    Add files via upload

---
+++ Upload.py
@@ -0,0 +1,8 @@
+import os
+
+filename = "PWM"
+
+os.system("avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o "+filename+".o "+ filename +".c \n")
+os.system("avr-gcc -mmcu=atmega328p "+filename+".o -o "+filename+" \n")
+os.system("avr-objcopy -O ihex -R .eeprom "+filename+" "+filename+".hex \n")
+os.system("avrdude -F -V -c arduino -p ATmega328P -carduino -P/dev/ttyUSB0 -b57600 -D -U flash:w:"+filename+".hex")
